4580fd6a988fcddb31f204c81ba2fd69184d0df8
thanasinpin <thanasin.pin@gmail.com>
2025-03-25 12:02:03 +0700

    second commit

--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/widgets/custom_scaffold.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(child: Container(
+            child: Text("Welcome"),
+            )),
+            Flexible(child: Text("Welcome"),
+            ],
+            ),
+      ),
+    );
   }
 }
